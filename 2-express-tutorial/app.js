6bbbc6459a9a2380ad1eb0f1b83d66a4e7b27616
broncs <jhowfrombrazil@gmail.com>
2021-03-27 10:15:22 -0300

    middleware urlencoded

--- 2-express-tutorial/app.js
+++ 2-express-tutorial/app.js
@@ -1,58 +1,24 @@
 const express = require('express');
 const app = express();
+let { people } = require('./data');
 
-const { products } = require('./data');
+app.use(express.static('./methods-public'));
+// parse form data
+app.use(express.urlencoded({ extended: false }));
 
-// app.use(express.static('./public'));
-
-app.get('/', (req, res) => {
-  res.send('<h1> Home page </h1><a href="/api/products">products</a>');
+app.get('/api/people', (req, res) => {
+  res.status(200).json({ success: true, data: people });
 });
 
-app.get('/api/products', (req, res) => {
-  const newProducts = products.map((product) => {
-    const { id, name, image } = product;
-    return {
-      id,
-      name,
-      image,
-    };
-  });
-  res.json(newProducts);
-});
-
-app.get('/api/products/:productID', (req, res) => {
-  const { productID } = req.params;
-
-  const singleProduct = products.find(
-    (product) => Number(productID) === product.id
-  );
-
-  if (!singleProduct) {
-    return res.status(404).send('Product does not exist');
-  }
-  return res.json(singleProduct);
-});
-
-app.get('/api/b1/query', (req, res) => {
-  const { search, limit } = req.query;
-  let sortedProducts = [...products];
-
-  if (search) {
-    sortedProducts = sortedProducts.filter((product) => {
-      return product.name.startsWith(search);
-    });
+app.post('/login', (req, res) => {
+  const { name } = req.body;
+  if (name) {
+    return res.status(200).send(`Welcome ${name}`);
   }
 
-  if (limit) {
-    sortedProducts = sortedProducts.slice(0, Number(limit));
-  }
-  if (sortedProducts.length < 1) {
-    return res.status(200).json({ success: true, data: [] });
-  }
-  res.send(sortedProducts);
+  res.status(401).send('Please provide credentials');
 });
 
 app.listen(5000, () => {
-  console.log('the server is running on port 5000');
+  console.log('server is listening on port 5000...');
 });
